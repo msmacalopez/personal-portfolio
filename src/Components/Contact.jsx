c844d02e220e7d01709adfaf5e0a4204735b32fb
msmacalopez <msmacalopezs@gmail.com>
2025-10-26 00:34:27 +1100

    added contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Contact() {
   return (
     <div id="contact" className="section contact">
-      Contact
+      <div className="card-container">
+        <div className="card-contact">
+          <div className="card-front">Macarena Lopez</div>
+          <div className="card-back">contact_msmaca@gmail.com</div>
+        </div>
+      </div>
     </div>
   );
 }
