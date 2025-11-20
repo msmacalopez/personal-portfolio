9a8ce43cdbe2621aefcba13997a5c796218c3ed4
msmacalopez <msmacalopezs@gmail.com>
2025-11-20 15:01:58 +1100

    new background

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../styles/hero.css";
+import videobg from "../assets/cosmo.mp4";
 
 export default function Hero() {
   return (
     <div id="hero" className="section hero">
-      <div className="hero-bg"></div>
+      {/* to add picture in CSS */}
+      {/* <div className="hero-bg"></div>  */}
+      <video
+        src={videobg}
+        className="bg-video"
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
       <div className="hero-content">
         <h3 className="h3-hero">I Code...</h3>
         <h1 className="h1-hero">Macarena Lopez</h1>
